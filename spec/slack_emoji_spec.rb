bff94888321a8968b2ed069a48a0721e18e1748f
dnond <tarosuk@gmail.com>
2016-06-29 22:54:52 +0900

    Fix spec

--- spec/slack_emoji_spec.rb
+++ spec/slack_emoji_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe SlackEmojiList do
+describe SlackEmoji do
   it 'has a version number' do
-    expect(SlackEmojiList::VERSION).not_to be nil
+    expect(SlackEmoji::VERSION).not_to be nil
   end
 
   it 'does something useful' do
